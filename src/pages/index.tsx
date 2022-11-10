18f6518b29ee99542aa0f8b5849d2707dcc386cb
Pedro Ferreira <pedrojunior271@gmail.com>
2022-11-09 23:50:51 -0300

    test commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import style from '../styles/home.module.css'
+import Head from 'next/head'
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Início | ig.news</title>
+    </Head>
     <h1 className={style.title}>Home</h1>
+    </>
   )
 }
